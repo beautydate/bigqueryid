d7605d66878c69ef3987e816b2bcd14de2c80285
Leonardo Saraiva <vyper@maneh.org>
2017-01-09 17:01:29 -0200

    Add method .where to search rows

--- lib/bigqueryid/criteria/queryable.rb
+++ lib/bigqueryid/criteria/queryable.rb
@@ -6,6 +6,24 @@ module Bigqueryid
 
       class_methods do
 
+        def where(filter)
+          return [] if filter.empty?
+
+          where = []
+          filter.each do |key, value|
+            filter[key] = coercer.coerce(key, value)
+            where << "#{key} = @#{key}"
+          end
+
+          query = "SELECT * FROM #{dataset_name}.#{table_name}" \
+                  " WHERE #{where.join(' AND ')}"
+
+          result = bigquery.query(query, params: filter)
+          result.map do |row|
+            new(row.select { |f| attributes.include?(f) })
+          end
+        end
+
         def run(query)
           result = bigquery.query query
           new(result.first) if result.count == 1
